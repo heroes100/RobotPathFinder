cd9a49c71b00513c684a3bab7c41a52202ff157e
Mark Nijjar <mark_nijjar@hotmail.com>
2017-04-08 11:41:26 -0400

    Added recursive depth first search algorithm.

--- robot.py
+++ robot.py
@@ -1,10 +1,15 @@
 import sys
+import math
+import copy
+
+minPathTime = 999999999
 
 #Constants
 robotSpeed = 0.2
 waypointDelay = 10
-endPosition = [100, 100]
+endPosition = (100, 100, 0)
 delayMatrix = []
+penaltyMatrix = []
 
 #Robot class used for spawning test robots to follow a path
 class Robot:
@@ -20,51 +25,64 @@ class Robot:
 
     #Finds the hypotnuse of newX and newY
     def CalculateTime(self, newX, newY):
-        return (((newX**2 + newY**2))**(1/float(2))) * robotSpeed
+        global robotSpeed
+        return math.sqrt(((newX**2 + newY**2))) * robotSpeed
 
     #This function takes in a waypoint index for the
     # robot to traverse to and returns the total time required
     # for the robot to get there (plus penalty)
     def GetWaypointTravelTime(self, waypointIndex):
-        penaltyTime = 0
         #Find the time required to travel to new X,Y location
-        timeReq = self.CalculateTime(self.waypoints[waypointIndex][0] - self.Loc[0], self.waypoints[waypointIndex][1] - self.Loc[1])
+        timeReq = self.CalculateTime(self.Loc[0] - self.availWP[waypointIndex][0], self.Loc[1] - self.availWP[waypointIndex][1])
+                    
+        return timeReq
+
+    def GetWaypointPenaltyTime(self, waypointIndex):
+        penaltyTime = 0
         #Calculate the possible penalties
         for penaltyIndex in range(self.WPIndex+1, waypointIndex):
-            penaltyTime += self.waypoints[penaltyIndex][2]
-            
-        return (timeReq + penaltyTime)
+            penaltyTime += self.availWP[penaltyIndex][2]
+        return penaltyTime
 
-
+    def GetWaypointTotalTime(self, waypointIndex):
+        return (self.GetWaypointTravelTime(waypointIndex)
+                    + self.GetWaypointPenaltyTime(waypointIndex) )
 
     #Move the robot to the indexed waypoint location and update stats
     def TravelToWaypointIndex(self, index):
         global waypointDelay
-        self.timer += self.GetWaypointTravelTime(index) + waypointDelay
-        self.Loc = [self.waypoints[index][0], self.waypoints[index][1]]
+        self.timer += self.GetWaypointTotalTime(index) + waypointDelay
+        self.Loc = [self.availWP[index][0], self.availWP[index][1]]
         self.WPIndex = index
         self.availWP = self.availWP[index+1:]
+
 
     def TravelToEndPoint(self):
         global endPosition
         #Add end point to array
-        self.availWP.append([(endPosition[0], endPosition[1],0)])
+        self.availWP.append(((endPosition[0], endPosition[1],0)))
         #Travel to end point
-        self.timer += self.GetWaypointTravelTime(len(self.availWP)-1)
+        self.timer += self.GetWaypointTotalTime(len(self.availWP)-1)
         self.Loc = [endPosition[0], endPosition[1]]
         self.WPIndex = -2
         self.availWP = []
 
+    def AppendEndPoint(self):
+        global endPosition
+        self.availWP.append((endPosition[0], endPosition[1],0))
+        self.waypoints.append((endPosition[0], endPosition[1],0))
+
         
 
 def ParseMap(filePtr):
-    global delayMatrix
+    global delayMatrix, penaltyMatrix
     waypoints = []
     numCheckpoints = filePtr.readline();
     for i in range(int(numCheckpoints)):
         (x, y, p) = filePtr.readline().split(" ")
         waypoints.append( (int(x), int(y), int(p)) )
-    delayMatrix = [[-1 for x in range(len(waypoints))] for x in range(len(waypoints))]
+    delayMatrix = [[-1 for x in range(len(waypoints)+1)] for x in range(len(waypoints)+1)]
+    penaltyMatrix = [-1 for x in range(len(waypoints)+1)]
     return waypoints
 
 #if __name__ == "__main__":
@@ -76,15 +94,24 @@ def ParseMap(filePtr):
 #    waypoints = ParseMap(filePtr)
 
 
-def FillDelayArray():
-    global delayMatrix
+def FillDelayMatrix():
+    global delayMatrix, penaltyMatrix, endPosition
     robot1 = Robot(waypoints)
-    for i in range(len(waypoints)):
-        for j in range(len(waypoints)):
-            delayMatrix[i][j] = robot1.GetWaypointTravelTime(j)
+    robot1.availWP.append((endPosition[0], endPosition[1],0))
+    robot1.waypoints.append((endPosition[0], endPosition[1],0))
+    for i in range(len(robot1.waypoints)):
+        for j in range(i, len(robot1.waypoints)):
+            delayMatrix[i][j] = robot1.GetWaypointTravelTime(j-i)
+        penaltyMatrix[i] = robot1.availWP[0][2]
         robot1.TravelToWaypointIndex(0)
+        
+
+def pm(m):
+    for i in range(len(m)):
+        print m[i]
 
 
+            
 # Create X number of threads for calculating paths
 # Each thread will calculate a minimum time
 # If a path exceeds the current minimum path time, STOP following it
@@ -97,17 +124,46 @@ def FillDelayArray():
 #   else
 #       Return as invalid path
 def GeneratePaths(robot):
+    global minPathTime, waypointDelay
     #For every available waypoint remainin
     #for path in range(len(robot.availWP)):
-     
-    return
+    index = 0
+    pathTime = 99999
+    finishTime = 999999999
+    for wp in range(len(robot.availWP)):
+        #If this path might be shorter
+        if(robot.timer + robot.GetWaypointTotalTime(wp) + waypointDelay < minPathTime):
+            try:
+                newRobot = copy.deepcopy(robot)
+            except:
+                print "Failed to make a new robot in recursive call! Waypoint: {}".format(wp)
+                print "Index: {}".format(index)
+                return None
+            newRobot.TravelToWaypointIndex(wp)
+            #Recursive call: GeneratePaths(newRobot)
+            index, pathTime = GeneratePaths(newRobot)
+            if(pathTime < finishTime):
+                finishTime = pathTime
+        #else:
+            #print "Path {} not worth taking.".format(wp)
+            
+    if(robot.Loc == [endPosition[0], endPosition[1]]):
+        if(robot.timer - waypointDelay < minPathTime):
+            print "Robot at the end! Timer: {}".format(robot.timer)
+            minPathTime = robot.timer - waypointDelay
+            finishTime = robot.timer - waypointDelay
+        return (index, finishTime)
+        #If we made it to the end, calculate time and update minPathTime
+        #Return data
+        
+    return (index, finishTime)
 
 
 def DijkstraPath():
     global waypoints
     robot1 = Robot(waypoints)
     for i in range(len(waypoints)):
-        robot1.TravelToWaypointIndex(i)
+        robot1.TravelToWaypointIndex(0)
     robot1.TravelToEndPoint()
     print robot1.timer
     
@@ -122,6 +178,10 @@ def PrintWaypoints():
         
 
 #####Debuging 
-filePtr = open('sample_input_medium.txt', 'r')
+filePtr = open('sample_input_large.txt', 'r')
+waypoints = ParseMap(filePtr)
 waypoints = ParseMap(filePtr)
 availWP = waypoints[:]
+
+r = Robot(waypoints)
+r.AppendEndPoint()
